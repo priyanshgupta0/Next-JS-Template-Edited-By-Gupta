2d33cd0d72fa84dcd49e34e67cd8826f15d91b56
priyanshgupta0 <pg167783@gmail.com>
2024-04-26 18:23:40 +0530

    Add dependencies and update global styles

---
+++ src/app/about/page.tsx
@@ -0,0 +1,21 @@
+import AboutSectionOne from "@/components/About/AboutSectionOne";
+import AboutSectionTwo from "@/components/About/AboutSectionTwo";
+
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About Page ",
+  description: "This is About Page for GuptaJi",
+  // other metadata
+};
+
+const AboutPage = () => {
+  return (
+    <>
+      <AboutSectionOne />
+      <AboutSectionTwo />
+    </>
+  );
+};
+
+export default AboutPage;
